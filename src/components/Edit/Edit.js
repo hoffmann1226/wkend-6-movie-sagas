5055902a1d3dad42019d57833e1fb0f0f76c3737
Tom Hoffmann <hoffmann1226@gmail.com>
2020-07-25 16:00:59 -0500

    edit page basic framework setup

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Edit extends Component {
-  // Renders the entire app on the DOM
+
+  goBack = () => {
+    // console.log('handleClick Back To List')
+    this.props.history.push('/details');
+}
   render() {
     return (
       <div>
-       <p>Edit</p>
+       <p>Edit Movie Details</p>
+       <input placeholder="new title"></input>
+       <input placeholder="new description"></input>
+       <button onClick={this.goBack}>Cancel</button>
+       <button onClick={this.saveChanges}>Save</button>
       </div>
     );
   }
