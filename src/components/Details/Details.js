230f91c5f2d2b2732275d0fe24a8870e0617d14a
Tom Hoffmann <hoffmann1226@gmail.com>
2020-07-25 15:52:08 -0500

    genres and other details displayed on details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -18,7 +18,17 @@ goEdit = () => {
       <div>
        <button onClick={this.goBack}>Back to List</button>
        <button onClick={this.goEdit}>Edit</button>
-       {JSON.stringify (this.props.reduxState.detailsReducer)}
+       {/* {JSON.stringify (this.props.reduxState.detailsReducer)} */}
+       {this.props.reduxState.detailsReducer.map(movie => {
+          return (
+            <div key={movie.id}>
+              <p>{movie.title}</p>
+              <img src={movie.poster} alt="movie" onClick={()=>this.handleClick(movie.id)}/>
+              <p>{movie.description}</p>
+              <p>Genres: {movie.genre_list}</p>
+            </div>
+          )
+        })}
       </div>
     );
   }
